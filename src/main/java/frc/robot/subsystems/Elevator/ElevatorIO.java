75ba0daf819a2d60b2654a4dfed895dc0fe11120
DepressedFrog12 <pranav.paati@gmail.com>
2025-01-10 20:15:41 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorIO.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.Elevator;
+import org.littletonrobotics.junction.AutoLog;
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+import frc.robot.Constants;
+
+import static frc.robot.Constants.*;
+
+public interface ElevatorIO {
+    public static double elevatorRightID = Constants.ElevatorConstants.elevatorRightID;
+    public static double elevatorLeftID = Constants.ElevatorConstants.elevatorLeftID;
+
+    public abstract void setVolts();
+    public abstract void updateInputs();
+
+    @AutoLog
+    public static class ElevatorInputs {
+        public double elevatorRightVelocity = 0;
+        public double elevatorLeftVelocity = 0;
+
+        public double elevatorRightDistance = 0;
+        public double elevatorLeftDistance = 0;
+
+        public double elevatorRightAmps = 0;
+        public double elevatorLeftAmps = 0;
+    }
+}
